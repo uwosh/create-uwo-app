65f7ac6d1094a291d2dd277b7c22b6db360887db
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-07-10 16:34:54 -0500

    wordmark and site title are present

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./Header.css";
 import TopNav from "./TopNav/TopNav";
+import HeaderMain from "./HeaderMain/HeaderMain";
 
 class Header extends Component {
   render() {
     return (
       <div className="header-wrapper">
         <TopNav />
+        <HeaderMain />
       </div>
     );
   }
---
+++ src/components/Header/HeaderMain/HeaderMain.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./HeaderMain.css";
+import Wordmark from "../../../images/wordmark.png";
+
+class HeaderMain extends Component {
+  render() {
+    return (
+      <div className="header-main-wrapper">
+        <div className="header-main-content">
+          <img
+            src={Wordmark}
+            alt="University of Wisconsin Oshkosh logo"
+            className="wordmark"
+          />
+          <div className="header-main-content-text">
+            <div className="site-title">UW Oshkosh</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HeaderMain;

e3a532e52717730b1f05c62f107fcee839d5eec5
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-07-16 08:28:13 -0500

    Sticky footer positioning

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import "./Body.css";
 
 class Body extends Component {
   render() {
     return (
       <div className="body-wrapper">
-        <h1>Body Title</h1>
-        <p>Body text.</p>
+        <main className="content-wrapper">
+          <h1>Body Title</h1>
+          <p>Body text.</p>
+        </main>
       </div>
     );
   }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import "./Footer.css";
 
 class Footer extends Component {
   render() {
     return (
-      <div className="footer-wrapper">
+      <footer className="footer-wrapper">
         <h1>Footer Title</h1>
         <p>Footer text.</p>
-      </div>
+        <p>Footer text 2.</p>
+      </footer>
     );
   }
 }

964db3d88c5de94f3e59b2a28604c5243f800e1e
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-07-23 13:44:11 -0500

    Basic structure of the resources area set up

--- src/components/Footer/FooterMain/Resources/Resources.js
+++ src/components/Footer/FooterMain/Resources/Resources.js
@@ -3,7 +3,29 @@ import "./Resources.css";
 
 class Resources extends Component {
   render() {
-    return <h1>this is resources</h1>;
+    return (
+      <div className="resources-wrapper">
+        <div className="resources-title title">Resources</div>
+        <ul>
+          <li>
+            <a href="http://www.uwosh.edu/resources/accessibility">
+              Accessibility
+            </a>
+          </li>
+          <li>
+            <a href="https://www.google.com/maps?q=University+of+Wisconsin+Oshkosh+800+Algoma+Blvd.+Oshkosh,+WI+54901&um=1&ie=UTF-8&hl=en&sa=N&tab=wl">
+              Get Directions
+            </a>
+          </li>
+          <li>
+            <a href="https://umc.uwosh.edu/for-media/">Media Relations</a>
+          </li>
+          <li>
+            <a href="https://hr.uwosh.edu/careers">Work at UW Oshkosh</a>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 

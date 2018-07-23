06b93d8d75fbf265f9465964da8dd7b3e75066b0
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-07-23 13:52:51 -0500

    basic badges set up

--- src/components/Footer/FooterMain/Badges/Badges.js
+++ src/components/Footer/FooterMain/Badges/Badges.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 import "./Badges.css";
+import UWSystemBadge from "./images/uw-system.png";
 
 class Badges extends Component {
   render() {
-    return <h1>This is badges</h1>;
+    return (
+      <div className="badges-wrapper">
+        <div className="accreditation-wrapper">
+          <iframe
+            title="accreditation status badge for UW Oshkosh"
+            frameborder="0"
+            scrolling="no"
+            allowtransparency="true"
+            width="150"
+            height="166"
+            src="https://cdn.yoshki.com/iframe/54732.html"
+          />
+        </div>
+        <div className="uw-system-badge-wrapper">
+          <img src={UWSystemBadge} alt="University of Wisconsin System Badge" />
+        </div>
+      </div>
+    );
   }
 }
 
